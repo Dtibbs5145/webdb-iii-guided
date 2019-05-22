ce908483a5039d286332fa8396e9073c3120e8df
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-05-22 15:17:31 -0400

    saving guide

---
+++ migrations/20190522135333_create_roled_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('roles', table => {
+      table.increments();
+      table.string('name', 256).notNullable().unique() // second argument is optional
+      table.timestamps(true, true); // optional table
+  })
+};
+
+
+// undoes what is done above and will run when rollback is done
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('roles')
+};
